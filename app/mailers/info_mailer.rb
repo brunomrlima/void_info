b9cab14378142f623c5cb5cebae96fa373da1bdf
brunomrlima <bruno.mrlima@gmail.com>
2020-05-07 14:46:48 -0400

    emailing working

--- app/mailers/info_mailer.rb
+++ app/mailers/info_mailer.rb
@@ -1,4 +1,5 @@
 class InfoMailer < ApplicationMailer
+  include ActionView::Helpers::NumberHelper
   def daily_covid_email(email)
     set_covid_info
     mail(to: email.email, subject: "Covid-19 Info Today")
@@ -11,14 +12,14 @@ class InfoMailer < ApplicationMailer
       country_preferences.each do |country|
         hash = {}
         data_covid = country.data_covids.last
-        hash[:country] = country
-        hash[:total_cases] = data_covid.total_cases
-        hash[:new_cases] = data_covid.new_cases
-        hash[:total_deaths] = data_covid.total_deaths
-        hash[:new_deaths] = data_covid.new_deaths
-        hash[:total_recovered] = data_covid.total_recovered
-        hash[:active_cases] = data_covid.active_cases
-        hash[:critical_cases] = data_covid.critical_cases
+        hash[:country] = country.name
+        hash[:total_cases] = number_with_delimiter(data_covid.total_cases)
+        hash[:new_cases] = number_with_delimiter(data_covid.new_cases)
+        hash[:total_deaths] = number_with_delimiter(data_covid.total_deaths)
+        hash[:new_deaths] = number_with_delimiter(data_covid.new_deaths)
+        hash[:total_recovered] = number_with_delimiter(data_covid.total_recovered)
+        hash[:active_cases] = number_with_delimiter(data_covid.active_cases)
+        hash[:critical_cases] = number_with_delimiter(data_covid.critical_cases)
         @array << hash
       end
 
--- lib/tasks/send_info.rake
+++ lib/tasks/send_info.rake
@@ -4,9 +4,8 @@ namespace :send_info do
   task :covid_info => :environment do
     emails = Email.all
     emails.find_each do |email|
-
+      InfoMailer.daily_covid_email(email).deliver_now
     end
-
   end
 
 end
