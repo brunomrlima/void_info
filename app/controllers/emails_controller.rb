a0520a0337ae10552118c7e232537f4dd71a01cc
brunomrlima <bruno.mrlima@gmail.com>
2020-05-07 18:33:55 -0400

    now working

--- app/controllers/emails_controller.rb
+++ app/controllers/emails_controller.rb
@@ -4,6 +4,21 @@ class EmailsController < ApplicationController
   end
 
   def create
-
+    email = Email.new(emails_params)
+    if email.is_new_email?
+      if email.save!
+        flash[:notice] = "You successfully subscribed!"
+      else
+        flash[:danger] = "Something went wrong... Try again."
+      end
+    else
+      flash[:danger] = "This email is already subscribed."
+    end
+    redirect_to root_path
   end
+
+  private
+    def emails_params
+      params.require(:email).permit(:email)
+    end
 end
--- app/models/email.rb
+++ app/models/email.rb
@@ -1,2 +1,5 @@
 class Email < ApplicationRecord
+  def is_new_email?
+    return Email.find_by(email: self.email).blank?
+  end
 end
