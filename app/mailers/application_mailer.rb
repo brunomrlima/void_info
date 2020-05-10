70e6efc3959638454796b539a505542805639f32
brunomrlima <bruno.mrlima@gmail.com>
2020-05-10 17:10:14 -0400

    changing default email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'realvoidinfo@gmail.com'
+  # default from: 'realvoidinfo@gmail.com'
+  default from: 'daily@voidinfo.com'
   layout 'mailer'
 end
