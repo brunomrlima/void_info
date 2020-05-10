01694ab14376c27c4d282adab6b84a97e4c49afe
brunomrlima <bruno.mrlima@gmail.com>
2020-05-10 17:23:38 -0400

    updating email

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,5 +1,5 @@
 class ApplicationMailer < ActionMailer::Base
   # default from: 'realvoidinfo@gmail.com'
-  default from: 'daily@voidinfo.com'
+  default from: 'info@voidinfo.com'
   layout 'mailer'
 end
