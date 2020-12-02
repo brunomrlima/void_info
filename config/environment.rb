89af8a0c778a759e18b266dec49fa7ed30fd9c47
brunomrlima <bruno.mrlima@gmail.com>
2020-12-02 13:27:29 -0500

    adding api key for sendgrid

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,8 @@ require_relative 'application'
 Rails.application.initialize!
 
 ActionMailer::Base.smtp_settings = {
-    :user_name => Figaro.env.sendgrid_username,
-    :password => Figaro.env.sendgrid_password,
+    :user_name => 'apikey',
+    :password => Figaro.env.sendgrid_api_key,
     :domain => 'voidinfo.com',
     :address => 'smtp.sendgrid.net',
     :port => 587,
