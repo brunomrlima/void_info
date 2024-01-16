efb9b40887b898a479462dbdf4319a7e7fe674dd
brunomrlima <bruno.mrlima@gmail.com>
2024-01-16 15:59:23 -0500

    REmove webpacker and configure jsbundling-rails

--- config/webpack/webpack.config.js
+++ config/webpack/webpack.config.js
@@ -1,11 +1,28 @@
 const path    = require("path")
 const webpack = require("webpack")
+// Extracts CSS into .css file
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Removes exported JavaScript files from CSS-only entries
+// in this example, entry.custom will create a corresponding empty custom.js file
+const RemoveEmptyScriptsPlugin = require('webpack-remove-empty-scripts');
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
-    application: "./app/javascript/application.js"
+    application: [
+      "./app/javascript/application.js",
+      './app/assets/stylesheets/application.scss',
+    ],
+  },
+  module: {
+    rules: [
+      // Add CSS/SASS/SCSS rule with loaders
+      {
+        test: /\.(?:sa|sc|c)ss$/i,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+      },
+    ],
   },
   output: {
     filename: "[name].js",
@@ -13,9 +30,14 @@ module.exports = {
     chunkFormat: "module",
     path: path.resolve(__dirname, '..', '..', 'app/assets/builds'),
   },
+  resolve: {
+    extensions: ['.js', '.jsx', '.scss', '.css'],
+  },
   plugins: [
     new webpack.optimize.LimitChunkCountPlugin({
       maxChunks: 1
-    })
+    }),
+    new RemoveEmptyScriptsPlugin(),
+    new MiniCssExtractPlugin(),
   ]
 }
